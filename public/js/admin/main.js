5fbc042c595181cc8e5017960b833adb9ef0f566
Vova <79252214019@ya.ru>
2026-07-25 22:04:26 +0300

    Auto-deploy: 2026-07-25 19:04:26

--- public/js/admin/main.js
+++ public/js/admin/main.js
@@ -2,12 +2,21 @@ import { checkAuth, isAdmin } from '../api.js';
 import { initAdminItems } from './items.js';
 import { initAdminSettings } from './settings.js';
 
+function showLogin() {
+  const authModal = document.getElementById('authModal');
+  if (authModal) authModal.classList.add('open');
+  else location.href = '/#login';
+}
+
 checkAuth().then(() => {
   if (!isAdmin()) {
-    location.href = '/';
+    showLogin();
     return;
   }
   document.body.classList.remove('hidden');
   initAdminSettings();
   initAdminItems();
+}).catch(err => {
+  console.error('Auth check failed:', err);
+  showLogin();
 });
